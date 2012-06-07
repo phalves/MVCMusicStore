c4ccf35f3f9a25c0043ae8479394e1b940b78a9b
Paulo Henrique Alves <ph.alves@live.com>
2012-06-07 17:28:17 -0300

    Commit inicial

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        //
+        // GET: /Store/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album" + id };
+
+            return View(album);
+        }
+
+        //
+        // GET: /Store/Browse?genre=Disco
+
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre =" + genre);
+            return message;
+        }
+    }
+}
