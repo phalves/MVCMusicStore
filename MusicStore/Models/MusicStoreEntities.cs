b6d3b0bf177bacaacacf6d5e0471f7f7aa5c94b4
Paulo Henrique Alves <ph.alves@live.com>
2012-06-14 23:29:26 -0300

    5. Edit Forms using Scaffolding - Complete

--- MusicStore/Models/MusicStoreEntities.cs
+++ MusicStore/Models/MusicStoreEntities.cs
@@ -6,5 +6,6 @@ namespace MusicStore.Models
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
     }
 }
