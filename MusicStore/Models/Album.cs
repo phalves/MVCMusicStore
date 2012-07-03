6316c97c7b9b583d526c9a5071f245df3cd439d0
Paulo Henrique Alves <ph.alves@live.com>
2012-07-03 17:28:08 -0300

    6. Using Data Annotations for Model Validation - Complete

--- MusicStore/Models/Album.cs
+++ MusicStore/Models/Album.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace MusicStore.Models
 {
+    [Bind(Exclude = "AlbumId")]
     public class Album
     {
+        [ScaffoldColumn(false)]
         public int      AlbumId     { get; set; }
+
+        [DisplayName("Genre")]
         public int      GenreId     { get; set; }
+
+        [DisplayName("Artist")]
         public int      ArtistId    { get; set; }
+
+        [Required(ErrorMessage = "An Album Title is required")]
+        [StringLength(160)]
         public string   Title       { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 100.00,
+            ErrorMessage = "Price must be between 0.01 and 100.00")]
         public decimal  Price       { get; set; }
+
+        [DisplayName("Album Art URL")]
+        [StringLength(1024)]
         public string   AlbumArtUrl { get; set; }
         
         public Genre    Genre       { get; set; }
